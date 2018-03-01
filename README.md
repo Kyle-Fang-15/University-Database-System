61430e9bf2e3e37b4f048b58d4247564ba7d62a4
Kyle Y. K <yuankanfang@Yuankans-MacBook-Pro.local>
2018-02-28 19:27:18 -0800

    read me added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # University-Database-System
+# This is a project created to set up a student database system for general purposes.
---
+++ student.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class student {
+    private String firstName;
+    private String lastName;
+    private String gradeyear;
+    private int studentID;
+    private String courses;
+    private int tuitionBalanec;
+    private int costOfCOurse;
+
+
+}
