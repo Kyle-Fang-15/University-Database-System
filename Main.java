f0b63123ecee3dd0f84281244bb451e2070218c8
Kyle Y. K <yuankanfang@Yuankans-MacBook-Pro.local>
2018-03-01 21:00:53 -0800

    main method

--- Main.java
+++ Main.java
@@ -1,14 +1,27 @@
 package com.company;
 
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-    Student student1=new Student();
-    student1.enroll();
-    student1.payTuition();
-        System.out.println(student1.toString());
+
+        System.out.println("Please Enter The Number of Students: ");
+        Scanner in= new Scanner(System.in);
+        int numberOfStudents= in.nextInt();
+        Student[] students=new Student[numberOfStudents];
+
+        for (int i=0; i<numberOfStudents; i++){
+           students[i] = new Student();
+           students[i].enroll();
+           students[i].payTuition();
+            System.out.println(students[i].toString());
+
+        }
+
+
 
     }
 }
